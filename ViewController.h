f281ce42886d91a76091c40ec12ad5d423d1a225
alexxx beltrannn <58890076+beltradini@users.noreply.github.com>
2026-02-15 13:48:51 -0600

    Fix ResearchKit imports with compatibility header

--- ViewController.h
+++ ViewController.h
@@ -6,7 +6,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import <ResearchKit/ResearchKit.h>
+#import "ResearchKitSupport.h"
 
 @interface RootViewController : UIViewController <UIPageViewControllerDelegate>
 
